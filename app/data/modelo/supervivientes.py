b42125b4d00f14a72fa6599f9437dfa4ca471e19
roberto <roberto.ramirez1@educa.madrid.org>
2023-01-19 11:10:38 +0100

    demo

--- app/data/modelo/supervivientes.py
+++ app/data/modelo/supervivientes.py
@@ -1,7 +1,9 @@
 class Supervivientes:
-    def __init__(self, nombre_jugador : str, nombre_superviviente : str, color_de_pelo : str, complexion : str):
+    def __init__(self, nombre_jugador : str, nombre_superviviente : str, color_de_pelo : str, complexion : str, nombre_asesino : str, metros_por_segundo : int):
         self.nombre_jugador = nombre_jugador
         self.nombre_superviviente = nombre_superviviente
         self.color_de_pelo = color_de_pelo
         self.complexion = complexion
+        self.nombre_superviviente = nombre_asesino
+        self.metror_por_segundo = metros_por_segundo
         
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -15,3 +15,17 @@ def supervivientes():
 @rutas_usuarios.route("/asesinos")
 def asesinos():
     return render_template("asesinos.html")
+
+@rutas_usuarios.route('/formulario_supervivientes', methods=['POST'])
+def formulario_supervivientes():
+    nombre_jugador = request.form['nombre_jugador'] 
+    superviviente_favorito = request.form['superviviente_favorito'] 
+    color_de_pelo = request.form['color_de_pelo']
+    complexion = request.form['complexion']
+
+    cursor = db.database.cursor()  
+    sql = "INSERT INTO Supervivientes (Nombre,Apellido,CentroFormativo) VALUES (%s, %s, %s,%s)" 
+    data = (nombre_jugador,superviviente_favorito,color_de_pelo,complexion) 
+    cursor.execute(sql,data) 
+    db.database.commit() 
+
