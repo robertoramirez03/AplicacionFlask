46eb588f84bc6b8627da96b81199504f86204fc8
roberto <roberto.ramirez1@educa.madrid.org>
2023-02-03 09:42:30 +0100

    almost

--- app/data/jugadores_dao.py
+++ app/data/jugadores_dao.py
@@ -5,7 +5,7 @@ class Jugadores_dao:
 
     def select_all(self,db) -> list[Jugador]:
         cursor = db.cursor()
-        cursor.execute('SELECT Nombre_jugador, Nombre_Superviviente, Nombre_Asesino FROM rober.Jugadores INNER JOIN rober.Supervivientes on id_superviviente_favorito = Supervivientes.id INNER JOIN rober.Asesinos on id_asesino_favorito = Asesinos.id; ')
+        cursor.execute('SELECT Jugadores.id, Nombre_jugador, Nombre_Superviviente, Nombre_Asesino FROM rober.Jugadores INNER JOIN rober.Supervivientes on id_superviviente_favorito = Supervivientes.id INNER JOIN rober.Asesinos on id_asesino_favorito = Asesinos.id; ')
         jugadores_en_db = cursor.fetchall()
         jugadores_devueltos : list[Jugador] = list()
         for jugador_en_db in jugadores_en_db:
